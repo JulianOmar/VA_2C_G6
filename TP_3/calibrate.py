e25255f764580f6fd43a3709c30658e0cf0d9f40
Estudiante <pvazsquezpetracca@alumno.unlam.edu.ar>
2025-10-03 19:46:34 -0300

    Add Calibration in calibrate.cfg

--- TP_3/calibrate.py
+++ TP_3/calibrate.py
@@ -2,16 +2,15 @@ import numpy as np
 import cv2 as cv
 
 print("""
-      Usage:
-      Space: take picture
-      C: calibrate with taken pictures
-      ESC: quit
+      Uso:
+      Space: Tomar una foto
+      C: Calibrar con fotografías tomadas y guardar en calibrate.cfg
+      ESC: Salir
       """)
 
 ESC = chr(27)
 cv.namedWindow("Detecciones", cv.WINDOW_NORMAL)
-cv.namedWindow("Cam", cv.WINDOW_NORMAL)
-cv.namedWindow("Tablero", cv.WINDOW_NORMAL)
+cv.namedWindow("Camara", cv.WINDOW_NORMAL)
 defaultPrintOptions = np.get_printoptions()
 
 chessBoard = (9,6)
@@ -20,17 +19,14 @@ cornersubpixTerminationCriteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_I
 imgPoints = []
 objPoints = []
 
-# chessboard 3D points
+# Chessboard 3D points
 chessboardPointCloud3D = np.zeros((chessBoard[0]*chessBoard[1],3), np.float32)
 chessboardPointCloud3D[:,:2] = np.mgrid[0:chessBoard[0],0:chessBoard[1]].T.reshape(-1,2)
 
-#imChessboard = cv.imread("./assets/Chessboard_6x9.png")#, flags = cv.IMREAD_GRAYSCALE)
-#cv.imshow("Tablero", imChessboard) # Opens an empty window?
-
-cam = cv.VideoCapture(3)
+cam = cv.VideoCapture(2)
 width = cam.get(cv.CAP_PROP_FRAME_WIDTH)
 height = cam.get(cv.CAP_PROP_FRAME_HEIGHT)
-print("Cam resolution:", width, " x ", height)
+print("Resolucion Camara:", width, " x ", height)
 newSize = (640, int(640 * height / width))
 imBlack = np.zeros(newSize[::-1]+(3,), dtype=np.uint8)
 
@@ -39,11 +35,11 @@ while True:
     if ret:
         imLowRes = cv.resize(im, newSize)
         imGrayLowRes = cv.cvtColor(imLowRes, cv.COLOR_BGR2GRAY)
-        ret, corners = cv.findChessboardCorners(imGrayLowRes, chessBoard, None)
+        ret, corners = cv.findChessboardCorners(imGrayLowRes, chessBoard, None) 
         if ret:
             cv.drawChessboardCorners(imLowRes, chessBoard, corners, ret)
     
-    cv.imshow('Cam', imLowRes)
+    cv.imshow('Camara', imLowRes)
 
     key = cv.waitKey(33)
     if key>=0:
@@ -64,18 +60,19 @@ while True:
                     cv.drawChessboardCorners(imBlack, chessBoard, corners, ret)
                     cv.imshow("Detecciones", imBlack)
 
-                    print(len(imgPoints), "pictures taken")
+                    print(len(imgPoints), "fotografías tomadas.")
 
             case 'c':
                 # Calibra
                 ret, K, distCoef, rvecs, tvecs = cv.calibrateCamera(objPoints, imgPoints, im.shape[:2][::-1], None, None, flags=cv.CALIB_ZERO_TANGENT_DIST)
 
-                # Muestra resultados
-                print("Coeficientes de distorsión (K1, K2, P1, P2, K3):", distCoef)
-
-                np.set_printoptions(precision=2, suppress=True)
-                print("Matriz K", K)
-                np.set_printoptions(**defaultPrintOptions)
+                # Guarda resultados en archivo
+                coef_list = distCoef.flatten().tolist()
+                k_list = K.tolist()
+                with open("calibrate.cfg", "w") as f:
+                    f.write(f"coeficientes_distorsion = {coef_list}\n")
+                    f.write(f"matriz_k = {k_list}\n")
+                print("Resultados guardados en calibrate.cfg")
 
             case ESC:
                 print("Terminando.")
